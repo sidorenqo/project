d9dd2260f18a8e808df8da1c3d383aa8db1841a6
Gennady <sidorenqo@gmail.com>
2023-08-18 13:33:42 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// const result = confirm('Are you here?')
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer)
+
+// const answers = [];
+// answers[0] = prompt('Kak vas zovut', '')
+// answers[1] = prompt('Kakaya familiya', '')
+// answers[2] = prompt('Skolko vam let', '')
+
+// console.log(typeof(answers));
+
+
+
+// ************* Интерполяция **************
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`)
+
+const user = 'Gena'
+alert(`hello ${user}`)
